ce1ada03057c8cf61b12d750e1e267357c4a383c
John Pena <jpena91@gmail.com>
2019-01-06 23:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item){
-this.itemName = item;
-this.itemPrice = price;
   
-var price = Math.floor(Math.random()*101);
-
-cart.push(`itemName: ${item}, itemPrice: ${price}`);
-
-console.log(`${item} has been added to your cart.`);
-
-
 return cart;
   
 }
